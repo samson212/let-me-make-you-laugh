5bcfec3d3d124547a14cbbc8d63b342d7915b71b
Sam Lazarus <lazarus.sam@gmail.com>
2020-03-02 15:40:15 -0500

    committing laughDetector stub required for previous commit

---
+++ public/javascripts/modules/laughDetector.js
@@ -0,0 +1,39 @@
+const listeners = [],
+	detections = [],
+	thresholds = {};
+
+function init(opts={}) {
+	const defaultOpts = {
+		happyThreshold: 0.02,
+		surprisedThreshold: 0.02
+	};
+	for (const key in defaultOpts) {
+		if (!(key in opts)) {
+			opts[key] = defaultOpts[key];
+		}
+	}
+
+	thresholds.happy = opts['happyThreshold'];
+	thresholds.surprised = opts['surprisedThreshold'];
+
+	registerListener(e => console.log(`surpassed threshold for emotion '${e.tag}' with detection: ${JSON.stringify(e.detection)}`));
+}
+
+function registerDetection(detection) {
+	detections.push(detection);
+
+	for (const key in thresholds) {
+		if (detection[key] && detection[key].value > thresholds[key]) {
+			fireListeners({ tag: key, detection: detection[key] });
+		}
+	}
+}
+
+function registerListener(listener) { listeners.push(listener) }
+function fireListeners(emotion) { listeners.forEach(l => l(emotion)) }
+
+export default {
+	init: init,
+	onDetection: registerDetection,
+	addListener: registerListener
+}
